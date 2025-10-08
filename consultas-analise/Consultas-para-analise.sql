553b17e6a4096bb1ef18f1051bd4fa3db889163b
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2025-10-08 16:55:41 -0300

    Update Consultas-para-analise.sql

--- consultas-analise/Consultas-para-analise.sql
+++ consultas-analise/Consultas-para-analise.sql
@@ -1,4 +1,4 @@
--- ANÁLISE DE DADOS (algumas consultas iniciais)
+-- ANÁLISE DE DADOS (exemplos de consulta)
 
 -- Os cinco jogadores com mais vitórias 
 SELECT 
@@ -15,6 +15,7 @@ GROUP BY j.jogador_id, j.nome
 ORDER BY total_vitorias DESC
 LIMIT 5;
 
+
 -- As cinco aberturas mais utilizadas
 SELECT 
 	a.abertura_id,
@@ -26,7 +27,8 @@ LEFT JOIN partidas p
 GROUP BY a.abertura_id, a.nome_abertura
 ORDER BY qnt_vezes_usada DESC
 LIMIT 5;
-    
+
+
 -- Média de duração de partida por abertura
 SELECT a.nome_abertura, ROUND(AVG(p.duracao_minutos), 0) AS media_de_tempo_minutos
 FROM aberturas a
@@ -35,17 +37,20 @@ LEFT JOIN partidas p
 GROUP BY a.nome_abertura
 ORDER BY media_de_tempo_minutos DESC;
 
+
 -- Número de jogadores por país
 SELECT pais, COUNT(jogador_id) AS jogadores_por_país
 FROM jogadores
 GROUP BY pais
 ORDER BY jogadores_por_país DESC;
 
+
 -- Os cinco jogadores mais jovens
 SELECT nome, idade 
 FROM jogadores
 ORDER BY idade ASC
 LIMIT 5;
+
 
 -- Número de vitórias para as brancas por abertura
 SELECT a.nome_abertura, COUNT(p.resultado) AS vitorias_para_brancas
